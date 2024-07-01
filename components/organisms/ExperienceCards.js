811ff3589642bac5604536414a6767706c234d3e
JJ <jinjunoh2@gmail.com>
2024-07-01 13:57:39 -0500

    update resume with summer 2024

--- components/organisms/ExperienceCards.js
+++ components/organisms/ExperienceCards.js
@@ -3,18 +3,38 @@ import ExperienceCard from "../molecules/ExperienceCard";
 
 const experiences = [
   {
+    name: "LPL Financial",
+    position: "Software Engineer Intern",
+    date: "June 2024 - Present",
+    imgSrc: "lplfinancial.png",
+    works: [
+      "Generated OpenAPI specifications and .NET APIs for profile dashboard, utilized by 22,000+ financial advisors",
+      "Modernized ART ClientWorks application by UI implementations and migrating Angular.js from version 8 to 15",
+      "Adhered to Test-Driven Development to create unit tests for APIs using NUnit, achieving 90% code coverage",
+    ],
+  },
+  {
     name: "Sage Bionetworks",
     position: "Software Engineer Intern",
-    date: "Jan 2024 - Present",
+    date: "Jan 2024 - May 2024",
     imgSrc: "sagebionetworks.png",
-    works: ["Technology Platforms & Services team"],
+    works: [
+      "Developed and shipped Feature Flag service for Synapse, replacing 35+ experimental features utilizing AWS AppConfig and Java Servlet, providing real-time rollbacks, and decoupling code deployments from releases",
+      "Optimized rendering cycles by 25%, by migrating legacy GWT data query widgets to React.js and MUI library",
+      "Developed node-based visualization tools for data workflows in Synapse using React Flow and TypeScript",
+      "Enhanced Github Actions CI/CD pipeline by using Playwright to develop E2E test suites for Synapse Portals ",
+    ],
   },
   {
     name: "Texas Luminescence",
-    position: "Backend Developer",
+    position: "Software Developer",
     date: "Jul 2023 - Present",
     imgSrc: "texaslum.png",
-    works: ["Build Team: MusicMate"],
+    works: [
+      "Integrated Spotify’s OAuth workflow to automate fetching 13+ music metrics with React Native and Express.js",
+      "Built core REST APIs and token-based authentication system on Node.js and applied unit testing using Jest",
+      "Led a 6-member cross-functional team as project manager, successfully implementing Agile methodology by managing daily scrum meetings to synchronize and share working progress and conducting weekly sprint cycles",
+    ],
   },
   {
     name: "Jober",
